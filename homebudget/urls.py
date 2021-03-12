b682f145554e0488c2275dff1fff467cd97bb25b
Paulina Murzanska <p.murzanska@gmail.com>
2021-03-12 01:10:24 -0800

    some minor changes

--- homebudget/urls.py
+++ homebudget/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .import views
+from . import views
 
 router = routers.DefaultRouter()
 router.register("category", views.CategoryViewSet, basename="category")
