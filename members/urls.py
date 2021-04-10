59215debbc203f180278719121372c3a20c5bf4b
Paulina Murzanska <p.murzanska@gmail.com>
2021-04-10 03:04:00 -0700

    date format change

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserRegisterSuccessView
 
 
 urlpatterns = [
-    path("/register/", UserRegisterView.as_view(), name="register")
+    path("/register/", UserRegisterView.as_view(), name="register"),
+    path("/registered", UserRegisterSuccessView.as_view(), name="registered")
 
 ]
--- members/views.py
+++ members/views.py
@@ -1,8 +1,14 @@
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 
 
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = "register.html"
-    success_url = "http://localhost:3000/register_success"
+    success_url = reverse_lazy("registered")
+
+
+class UserRegisterSuccessView(TemplateView):
+    template_name = "registered.html"
